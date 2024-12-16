d5fedc1f8cc5b34417525cc9d6cefbe73651fddb
Lokesh Pawar <lokesh.pawar@dozee.io>
2024-12-17 04:36:12 +0530

    All the necessary functions are defined here

---
+++ src/pip_help/pip_func.py
@@ -0,0 +1,85 @@
+import subprocess
+import os
+from pathlib import Path
+from typing import TypeVar
+
+
+def log_dest_path() -> Path:
+    log_dest_path = os.path.join(os.path.expanduser("~"), "pip_help_logs")
+    if not os.path.exists(log_dest_path):
+        os.makedirs(log_dest_path)
+    Path.touch(f"{log_dest_path}/readme.md", exist_ok=True)
+    with open(f"{log_dest_path}/readme.md", "w") as file:
+        file.write(
+            "This directory contains logs of all the packages installed and removed using the pip-help command. Remove this directory if you are not using the pip-help command anymore."
+        )
+    return log_dest_path
+
+
+File = TypeVar("File", str, Path)
+
+
+def install(pip_package: str) -> None:
+    log_path = log_dest_path() + f"/pip_{pip_package}.txt"
+    package = subprocess.call(
+        ["pip", "install", pip_package, "--log", log_path, "--quiet"]
+    )
+    return package
+
+
+def installed_packages_list(pip_package: str) -> File:
+    target_path = log_dest_path() + f"/pip_{pip_package}.txt"
+    validation = False
+    with open(target_path, "r") as file:
+        possible_packages = []
+        dependent_packages = []
+
+        for line in file.readlines():
+            target_line = line.lower().split(" ")
+            word = "installing"
+            if word in target_line:
+                possible_packages.append(target_line)
+        # print(f"Possible packages: {possible_packages}")
+        if len(possible_packages) == 0:
+            print(
+                f'WARINING: Package {pip_package.upper()} is already installed. it is recommended to uninstall "{pip_package}" and related packages before installing it again. To uninstall "{pip_package}" and related packages, run the command: \'pip-help --remove/-r {pip_package}\''
+            )
+            os.remove(target_path)
+
+        else:
+            for i, k in enumerate(possible_packages[0]):
+                if k == "packages:":
+                    target_packages = possible_packages[0][i + 1 :]
+                    for target in target_packages:
+                        package_name = target.strip().split(",")[0]
+                        dependent_packages.append(package_name)
+            print(f"Installed packages: {dependent_packages}")
+
+            with open((log_dest_path() + f"/pip_{pip_package}_list.txt"), "w") as file:
+                try:
+                    for pkg in dependent_packages:
+                        file.write(f"{pkg}\n")
+                        validation = True
+                except Exception as e:
+                    print(f"Error: {e}")
+
+                if validation:
+                    os.remove(target_path)
+
+
+def uninstall(pip_package: str) -> None:
+    log_path = log_dest_path() + f"/pip_{pip_package}_list.txt"
+    if not os.path.exists(log_path):
+        raise TypeError(
+            f"The package {pip_package.upper()}, you are trying to remove is not installed."
+        )
+    else:
+        package_uninstalled = subprocess.call(
+            ["pip", "uninstall", "-r", log_path, "--yes"]
+        )
+        return package_uninstalled
+
+
+def delete_pip_cache() -> None:
+    clear_cache = subprocess.call(["pip", "cache", "purge"])
+    return clear_cache
